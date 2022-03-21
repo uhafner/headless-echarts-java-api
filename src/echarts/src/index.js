9dba6aa7a5f11404d750c9a3a79206554c2fc642
hmlimkokhoe <lim.kokhoe93@googlemail.com>
2022-03-21 13:32:38 +0100

    define static data and  chart rendering in own file using common-js export func

--- src/echarts/src/index.js
+++ src/echarts/src/index.js
@@ -1,39 +1,12 @@
 var http = require("http");
-var echarts = require("echarts");
-
-function renderChart() {
-  const chart = echarts.init(null, null, {
-    renderer: "svg",
-    ssr: true,
-    width: 400,
-    height: 300
-  });
-
-  chart.setOption({
-    xAxis: {
-      type: "category",
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    },
-    yAxis: {
-      type: "value"
-    },
-    series: [
-      {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: "bar"
-      }
-    ]
-  });
-
-  return chart.renderToSVGString();
-}
+var chartRenderer = require("./charts");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, {
       "Content-Type": "application/xml"
     });
-    res.write(renderChart());
+    res.write(chartRenderer.renderChart(800, 600));
     res.end();
   })
   .listen(8080);
